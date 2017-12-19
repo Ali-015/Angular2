647adfce09dfc8a20f7f413a271fbb254ce105b3
Ali Sadiq <Ali@Alis-MacBook-Pro.local>
2017-12-19 23:37:01 +0530

    UI changes for Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,16 @@ export class HomeComponent implements OnInit {
   public data: Subscription;
   public data1: any;
 
+
+  fillerNav = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'];
+  
+    fillerContent = Array(1).fill(0).map(() =>
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+         voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+         cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
   // Working with getters and setters
   ngOnInit() {
     this.data = this.service.get();
