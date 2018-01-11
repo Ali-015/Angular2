17a7b3be3ccee12bc20680dc1dba91ee3a06a382
Ali Sadiq <ali.sadiq@tavant.com>
2018-01-11 15:41:29 +0530

    Adding list in Product module

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,8 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  @ViewChild('details') details: ElementRef;
+  // @ViewChild('option') option: ElementRef;
+  @ContentChild ('option') option: ElementRef;
+
+  selectedCount = 0;
+  selectedOptions: string[] = ['Hola'];
+
+  maxCount = 4;
+
+  names = [
+    { CategoryID: 1,  CategoryName: "Beverages", Description: "Coffees, teas" },
+    { CategoryID: 2,  CategoryName: "Condiments", Description: "Sweet and savory sauces" },
+    { CategoryID: 3,  CategoryName: "Confections", Description: "Desserts and candies" },
+    { CategoryID: 4,  CategoryName: "Cheeses",  Description: "Smetana, Quark and Cheddar Cheese" },
+    { CategoryID: 5,  CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" },
+    { CategoryID: 6,  CategoryName: "Beverages", Description: "Beers, and ales" },
+    { CategoryID: 7,  CategoryName: "Condiments", Description: "Selishes, spreads, and seasonings" },
+    { CategoryID: 8,  CategoryName: "Confections", Description: "Sweet breads" },
+    { CategoryID: 9,  CategoryName: "Cheeses",  Description: "Cheese Burger" },
+    { CategoryID: 10, CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" }
+   ];
+
 
   ngOnInit() {
+    console.log(this.details);
+    console.log(this.option);
+  }
+  updateSelectedCount(count: number) {
+    this.selectedCount = count;
+    if (count === this.maxCount) {
+      alert('You have maxed out!');
+    }
+  }
+
+  onNgModelChange(event) {
+    console.log('on ng model change', event);
   }
 
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { productRouting } from './product-routing';
+import { MatListModule } from '@angular/material';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    productRouting
+    productRouting,
+    MatListModule,
+    FormsModule
   ],
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, SearchPipe],
   exports: [ProductComponent]
 })
 export class ProductsModule { }
