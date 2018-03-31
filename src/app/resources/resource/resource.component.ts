c2f0cc3bf6e6eafec5b50dbb35e2392f7d99cc56
Ali Sadiq <Ali@Alis-MacBook-Pro.local>
2018-03-31 11:14:34 +0530

    adding amcharts support

--- src/app/resources/resource/resource.component.ts
+++ src/app/resources/resource/resource.component.ts
@@ -9,6 +9,39 @@ export class ResourceComponent implements OnInit {
 
   constructor() { }
 
+
+  public options = {
+    "type": "gauge",
+    "theme": "light",
+    "axes": [ {
+    "axisThickness": 1,
+    "axisAlpha": 0.2,
+    "tickAlpha": 0.2,
+    "valueInterval": 20,
+    "bands": [ {
+    "color": "#84b761",
+    "endValue": 90,
+    "startValue": 0
+    }, {
+    "color": "#fdd400",
+    "endValue": 130,
+    "startValue": 90
+    }, {
+    "color": "#cc4748",
+    "endValue": 220,
+    "innerRadius": "95%",
+    "startValue": 130
+    } ],
+    "bottomText": "0 km/h",
+    "bottomTextYOffset": -20,
+    "endValue": 220
+    } ],
+    "arrows": [ {} ],
+    "export": {
+    "enabled": true
+    }
+  };
+
   ngOnInit() {
   }
 
--- src/app/resources/resources.module.ts
+++ src/app/resources/resources.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResourceComponent } from './resource/resource.component';
 import { ColorChangeDirective } from './color-change.directive';
+import { AmChartsModule } from "@amcharts/amcharts3-angular";
 
 @NgModule({
   imports: [
     CommonModule,
-    resourceRouting
+    resourceRouting,
+    AmChartsModule
   ],
   declarations: [ResourceComponent, ColorChangeDirective],
   exports: [ResourceComponent]
